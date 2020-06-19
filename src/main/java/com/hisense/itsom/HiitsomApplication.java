afb9f7ef92b808507660bba9e4e900f9ea5fc48a
lvxczhuce22 <938706428@qq.com>
2020-06-19 11:16:55 +0800

    t
    t

--- src/main/java/com/hisense/itsom/HiitsomApplication.java
+++ src/main/java/com/hisense/itsom/HiitsomApplication.java
@@ -33,6 +33,8 @@ public class HiitsomApplication {
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application HIITSOM is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+"Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+"Local: \t\thttp://localhost:" + port + path + "/\n\t" +
                 "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
                 "swagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
                 "Doc: \t\thttp://" + ip + ":" + port + path + "/doc.html\n" +
